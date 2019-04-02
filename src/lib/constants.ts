1a68d5cb1e1b9f7c64fe3b9811e106fc877a2f6b
eduard.titov <editied@gmail.com>
2019-04-02 13:12:38 +0300

    Update remote framepay links, Add no-cache prefix

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,13 +5,19 @@
  *
  */
 
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+/**
+ * Disable cache CDN links in development mode
+ */
+const LINK_CACHE_PREFIX = DEVELOPMENT ? `v=${new Date().getTime()}` : '';
 
 /**
  * Remote FramePay library CDN link.
  */
-export const FRAMEPAY_SCRIPT_LINK: string = 'http://localhost:8080/dist/rebilly.js';
+export const FRAMEPAY_SCRIPT_LINK: string = `http://localhost:8080/dist/rebilly.js?${LINK_CACHE_PREFIX}`;
 
 /**
  * Remote FramePay style CDN link.
  */
-export const FRAMEPAY_STYLE_LINK: string = 'https://cdn.rebilly.com/framepay/v1/rebilly.css';
+export const FRAMEPAY_STYLE_LINK: string = `https://cdn.rebilly.com/framepay/v1/rebilly.css?${LINK_CACHE_PREFIX}`;
