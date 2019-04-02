7dd3bcd2562a9c869292b7cc6758954c91e05ad7
eduard.titov <editied@gmail.com>
2019-04-02 11:02:43 +0300

    cosrstyle fix, add short react-native demo

--- example/parcel-react/src/Guide.js
+++ example/parcel-react/src/Guide.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 export default class Guide extends React.Component {
 
-  render() {
-    return (<div>
-      <h2>
-        <a href="#set-up-framepay-provider" aria-hidden="true" className="header-anchor">#</a>
-        Set up FramePayProvider
-      </h2>
-      <p>
-        See <a href="#">App.js#18</a>
-      </p>
-      <div className="language-html extra-class">
+    render() {
+        return (<div>
+            <h2>
+                <a href="#set-up-framepay-provider" aria-hidden="true" className="header-anchor">#</a>
+                Set up FramePayProvider
+            </h2>
+            <p>
+                See <a href="#">App.js#18</a>
+            </p>
+            <div className="language-html extra-class">
         <pre className="language-html"><code>{`<FramePayProvider settings={settings}>
 ...
 </FramePayProvider>`}</code></pre>
-      </div>
-    </div>);
-  }
+            </div>
+        </div>);
+    }
 }
