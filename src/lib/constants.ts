98251b636df410d809258e3787caab723907b4cc
eduard.titov <editied@gmail.com>
2019-04-07 23:53:35 +0300

    Setup dotenv

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,19 +5,14 @@
  *
  */
 
-const DEVELOPMENT = process.env.NODE_ENV === 'development';
-
-/**
- * Disable cache CDN links in development mode
- */
-const LINK_CACHE_PREFIX = DEVELOPMENT ? `v=${new Date().getTime()}` : '';
-
 /**
  * Remote FramePay library CDN link.
  */
-export const FRAMEPAY_SCRIPT_LINK: string = `http://localhost:8080/dist/rebilly.js?${LINK_CACHE_PREFIX}`;
+export const FRAMEPAY_SCRIPT_LINK: string =
+    process.env.FRAMEPAY_SCRIPT_LINK || '';
 
 /**
  * Remote FramePay style CDN link.
  */
-export const FRAMEPAY_STYLE_LINK: string = `https://cdn.rebilly.com/framepay/v1/rebilly.css?${LINK_CACHE_PREFIX}`;
+export const FRAMEPAY_STYLE_LINK: string =
+    process.env.FRAMEPAY_STYLE_LINK || '';
