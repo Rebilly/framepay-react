68097fc877d02e29684d7861b9de51c7b0e91a18
Eduard Titov <editied@gmail.com>
2019-04-09 19:40:02 +0300

    Update license, Add postinstall script (#3)
    * Update license, Add postinstall script
    
    * Update doc block for env file

--- env.js
+++ env.js
@@ -1,1 +1,8 @@
+/**
+ *
+ * Load the environment variables from .env into the process.env
+ * You can use `.env.local` to override it
+ * @see env.local.example
+ *
+ */
 require('dotenv').config();
