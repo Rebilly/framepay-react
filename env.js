ff5ede96cc225789f5366345267c60c1514c0c2d
eduard.titov <editied@gmail.com>
2019-04-09 19:37:43 +0300

    Update license, Add postinstall script

--- env.js
+++ env.js
@@ -1,1 +1,8 @@
+/**
+ *
+ * Load the environment variables from .env into the process.env
+ * Please use the .env.local to rewrite it
+ * @see env.local.example
+ *
+ */
 require('dotenv').config();
