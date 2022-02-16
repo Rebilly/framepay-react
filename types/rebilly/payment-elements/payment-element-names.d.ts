00e347b08163bbd02c3a6c26cc20916a8941175c
junyong <junyong.zeng@rebilly.com>
2022-02-16 09:40:10 +0800

    Remove digital wallet

--- types/rebilly/payment-elements/payment-element-names.d.ts
+++ types/rebilly/payment-elements/payment-element-names.d.ts
@@ -1,1 +1,1 @@
-type PaymentElements = 'card' | 'bankAccount' | 'iban' | 'applePay' | 'googlePay' | 'digitalWallet';
+type PaymentElements = 'card' | 'bankAccount' | 'iban' | 'applePay' | 'googlePay';
